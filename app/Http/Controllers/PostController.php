f2ffdadfa2e1f66ccc3c26fe3b01858713b64196
Clionegohan <sawakiguchik@gmail.com>
2024-07-22 14:32:18 +0900

    全角スペースの修正。PostControllerにて

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,18 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
-
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        //'getPaginateByLimit'メソッドを使用して、投稿をページネーションして取得
         return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
-        //brade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した&postを代入。
+        // getPaginateByLimit()はPost.phpで定義したメソッドです。
     }
 }
 ?>
-
